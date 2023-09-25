c21e0492d5bbfba43300b8d85f2f02dcce42074e
MinhThang <116664608+MinhThang1907@users.noreply.github.com>
2023-09-25 14:30:22 +0700

    Lab2

--- SachOnline/Controllers/SachOnlineController.cs
+++ SachOnline/Controllers/SachOnlineController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SachOnline.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,18 +9,31 @@ namespace SachOnline.Controllers
 {
     public class SachOnlineController : Controller
     {
+        QLBanSachEntities1 db = new QLBanSachEntities1();
+        private List<SACH> LaySachMoi(int count)
+        {
+            return db.SACHes.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        }
         // GET: SachOnline
         public ActionResult Index()
         {
-            return View();
+            var listSachMoi = LaySachMoi(6);
+            return View(listSachMoi);
         }
         public ActionResult ChuDePartial()
         {
-            return PartialView();
+            var listChuDe = from cd in db.CHUDEs select cd;
+            return PartialView(listChuDe);
+        }
+        public ActionResult NhaXuatBanPartial()
+        {
+            var listNhaXuatBan = from nxb in db.NHAXUATBANs select nxb;
+            return PartialView(listNhaXuatBan);
         }
         public ActionResult SachBanNhieuPartial()
         {
-            return PartialView();
+            var listSachMoi = LaySachMoi(6);
+            return PartialView(listSachMoi);
         }
     }
 }
